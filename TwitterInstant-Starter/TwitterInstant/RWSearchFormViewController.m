7ccf83591857a9cf04980e09b6925d15d4793507
jpcarreira <jpmavcarreira@gmail.com>
2014-03-29 19:59:38 +0000

    updating textfield background color considering is search text is valid

--- TwitterInstant-Starter/TwitterInstant/RWSearchFormViewController.m
+++ TwitterInstant-Starter/TwitterInstant/RWSearchFormViewController.m
@@ -8,6 +8,7 @@
 
 #import "RWSearchFormViewController.h"
 #import "RWSearchResultsViewController.h"
+#import <ReactiveCocoa.h>
 
 @interface RWSearchFormViewController ()
 
@@ -19,6 +20,7 @@
 
 @implementation RWSearchFormViewController
 
+
 - (void)viewDidLoad
 {
   [super viewDidLoad];
@@ -27,15 +29,45 @@
   
   [self styleTextField:self.searchText];
   
+    // locates the resultsViewController and assigns it to the resultsViewController property
+    // the major application logic is going to live within this class so this property will supply search results to RWSearchResultsViewController
   self.resultsViewController = self.splitViewController.viewControllers[1];
   
+    // text field background color using RAC
+    [self updateTextFieldBackgroundColorIfTextIsValid];
+    
 }
 
-- (void)styleTextField:(UITextField *)textField {
+
+- (void)styleTextField:(UITextField *)textField
+{
   CALayer *textFieldLayer = textField.layer;
   textFieldLayer.borderColor = [UIColor grayColor].CGColor;
   textFieldLayer.borderWidth = 2.0f;
   textFieldLayer.cornerRadius = 0.0f;
+}
+
+
+#pragma mark - RAC methods
+
+// takes the search field text, transforms ('maps') it to a UIColor and applies it to text field background color
+-(void)updateTextFieldBackgroundColorIfTextIsValid
+{
+    [[self.searchText.rac_textSignal map:^id(NSString *text){
+        return [self isValidSearchText:text] ? [UIColor whiteColor] : [UIColor yellowColor];
+    }]
+    subscribeNext:^(UIColor *color){
+        self.searchText.backgroundColor = color;
+    }];
+}
+
+
+#pragma mark - auxiliary methods
+
+// validating the search text to have at least 2 characters
+-(BOOL)isValidSearchText:(NSString *)text
+{
+    return text.length > 2;
 }
 
 @end
