8028c0c86d2b4a11bf568e93eb72d9d6f4033097
jpcarreira <jpmavcarreira@gmail.com>
2014-04-02 15:28:38 +0100

    loading images with RAC

--- TwitterInstant-Starter/TwitterInstant/RWSearchResultsViewController.m
+++ TwitterInstant-Starter/TwitterInstant/RWSearchResultsViewController.m
@@ -51,7 +51,42 @@
   cell.twitterStatusText.text = tweet.status;
   cell.twitterUsernameText.text = [NSString stringWithFormat:@"@%@",tweet.username];
   
+  // loading the images
+    
+    // first we reset the image because the cells are reusable and can contain stale data
+    cell.twitterAvatarView.image = nil;
+    
+    [[[self signalForLoadingImage:tweet.profileImageUrl]
+        deliverOn:[RACScheduler mainThreadScheduler]]
+    subscribeNext:^(UIImage *image){
+        cell.twitterAvatarView.image = image;
+    }];
+    
   return cell;
+}
+    
+    
+
+
+
+
+#pragma mark - RACSignal
+
+// loading an image using RAC
+-(RACSignal *)signalForLoadingImage:(NSString *)imageUrl
+{
+    // getting a background scheduler as we want to load images in a thread different than the main thread
+    RACScheduler *scheduler = [RACScheduler schedulerWithPriority:RACSchedulerPriorityBackground];
+    
+    // creating a signal to download the images and creating a UIImage when it has a subscriber
+    // (subscribeOn that ensures the signal is executed on the given scheduler)
+    return [[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:imageUrl]];
+        UIImage *image = [UIImage imageWithData:data];
+        [subscriber sendNext:image];
+        [subscriber sendCompleted];
+        return nil;
+    }] subscribeOn:scheduler];
 }
 
 @end
